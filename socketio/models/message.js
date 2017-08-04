df16559d83228d133906070f38a4062c96eab324
sydney <yongjing@dink.com.sg>
2017-08-04 18:24:28 +0800

    initial commit

--- socketio/models/message.js
+++
@@ -1,9 +0,0 @@
-
-var mongoose = require('mongoose');
-
-var ChatSchema = new mongoose.Schema({ 	
-  message: String,
-  created_at: { type: Date, default: Date.now},
-});
-
-module.exports = mongoose.model("Chat", ChatSchema);
